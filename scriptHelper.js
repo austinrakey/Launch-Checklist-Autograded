a8815f9028794385c3322de6982b483ca2cb31c4
Austin Rakey <rakeysound@gmail.com>
2024-03-16 20:19:07 -0500

    7/10

--- scriptHelper.js
+++ scriptHelper.js
@@ -24,26 +24,94 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     if (isNaN(testInput) === false)
     return "Is a Number"
     
-    if (isNaN(testInput) === true)
+    if (isNaN(testInput) === true) {
     return "Not a Number"
+    }
 }
  
 
 
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    let launchReady = true
+    let launchStatus = document.getElementById("launchStatus")
+    let launchStatusCheck = document.getElementById("launchStatusCheck")
+    // let faultyItems = document.getElementById("faultyItems")
+
+if (validateInput(pilot) === "Not a Number"){
+document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot} is ready for launch`
+list.style.visibility = "visible"
+} else {
+list.style.visibility = "visible"
+document.getElementById("pilotStatus").innerHTML = `Invalid Pilot Name!`
+launchReady=false
+}
+
+
+if (validateInput(copilot) === "Not a Number"){
+    document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot} is ready for launch`
+    list.style.visibility = "visible"
+    } else {
+    list.style.visibility = "visible"
+    document.getElementById("copilotStatus").innerHTML = `Invalid Copilot Name!`
+    launchReady=false
+    }
+
     
- }
+if (validateInput(fuelLevel) === "Is a Number"){
+        if (fuelLevel>=10000) {
+            document.getElementById("fuelStatus").innerHTML = `Fuel level high enough for launch`
+            list.style.visibility = "visible"
+        } else {
+            list.style.visibility = "visible"
+            document.getElementById("fuelStatus").innerHTML = `Fuel level too low for launch`
+            launchReady = false
+        }
+        } else {
+        list.style.visibility = "visible"
+        document.getElementById("fuelStatus").innerHTML = `Invalid Fuel Level Entry!`
+        launchReady=false
+        }
+
+if (validateInput(cargoLevel) === "Is a Number"){
+        if (cargoLevel<=10000) {
+            document.getElementById("cargoStatus").innerHTML = `Cargo mass low enough for launch`
+            list.style.visibility = "visible"
+        } else {
+            list.style.visibility = "visible"
+            document.getElementById("cargoStatus").innerHTML = `Cargo mass too heavy for launch`
+            launchReady = false
+        }
+        } else {
+        list.style.visibility = "visible"
+        document.getElementById("cargoStatus").innerHTML = `Invalid Cargo Mass Entry!`
+        launchReady=false
+        }
+
+if (launchReady === true){
+    launchStatus.innerHTML = 'Shuttle is Ready for Launch.'
+    launchStatus.style.color= 'green'
+} else {
+    launchStatus.innerHTML = "Shuttle Not Ready for Launch"
+    launchStatus.style.color = "red"
+}
+}
  
+
+
  async function myFetch() {
      let planetsReturned;
  
      planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json();
          });
-        //  return response.json();
-     return planetsReturned.response.json()
+     return planetsReturned
  }
  
- console.log(myFetch())
+//  (async () => {
+//     const planetsReturned = await myFetch();
+//     console.log(planetsReturned); // This will log the resolved value of the promise
+// })();
+
 
 
  function pickPlanet(planets) {
